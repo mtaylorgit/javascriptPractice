aedc362120de4cb3b4cd8258568fcc312d336d6d
oggie <morkthealien@gmail.com>
2023-12-28 15:22:46 -0600

    finished products section with buttons working

--- javascript-in-a-hurry-initial-main/main.js
+++ javascript-in-a-hurry-initial-main/main.js
@@ -255,18 +255,15 @@ thumb.addEventListener("click", function(e) {
 
 function productsHandler(){//run loop through products array and generate these items
 
+    //Products Section
     let productsSection = document.querySelector(".products-area");
 
-    //created these variables for using the buttons free, paid 
-    let freeProducts = products.filter(function(item){
-        return !item.price || item.price <= 0;
-    });
-    let paidProducts = products.filter(function(item){
-        return item.price > 0;
-    });
+    function populateProducts(productList) {
 
-    //run a loop through the products and create an HTML element ("product-item") for each of them
-    products.forEach(function(product,index){
+        productsSection.textContent = ""; //this clears so that every time you hit a button it doesn't keep adding pictures
+
+         //run a loop through the products and create an HTML element ("product-item") for each of them
+    productList.forEach(function(product,index){
         //Create the HTML element for the individual product
         let productElm = document.createElement("div");
         productElm.classList.add("product-item");
@@ -311,16 +308,93 @@ function productsHandler(){//run loop through products array and generate these 
         //Send all the complete individual product to the product section
         productsSection.append(productElm);
     });
+    }
 
+    // let productsSection = document.querySelector(".products-area");
+
+    //created these variables for using the buttons free, paid 
+    let freeProducts = products.filter(function(item){
+        return !item.price || item.price <= 0;
+    });
+    let paidProducts = products.filter(function(item){
+        return item.price > 0;
+    });
+
+    // //run a loop through the products and create an HTML element ("product-item") for each of them
+    // products.forEach(function(product,index){
+    //     //Create the HTML element for the individual product
+    //     let productElm = document.createElement("div");
+    //     productElm.classList.add("product-item");
+        
+    //     //Create the product image
+    //     let productImage = document.createElement("img");
+    //     productImage.src = product.image;
+    //     productImage.alt = "Image for " + product.title;//did this because they don't have alt
+
+    //     //Create the product details section
+    //     let productDetails = document.createElement("div");
+    //     productDetails.classList.add("product-details");
+
+    //     //Create Product Title (the H3), author, price-title, and price (which are 3 Ps)
+    //     let productTitle = document.createElement("h3");
+    //     productTitle.classList.add("product-title");
+    //     productTitle.textContent = product.title;
+
+    //     //these are the P tags
+    //     let productAuthor = document.createElement("p");
+    //     productAuthor.classList.add("product-author");
+    //     productAuthor.textContent = product.author;
+
+    //     let priceTitle = document.createElement("p");
+    //     priceTitle.classList.add("price-title");
+    //     priceTitle.textContent = "Price";
+
+    //     let productPrice = document.createElement("p");
+    //     productPrice.classList.add("product-price");
+    //     productPrice.textContent = product.price > 0 ? "$" + product.price.toFixed(2) : "Free";
+
+    //     //Append the product details
+    //     productDetails.append(productTitle);
+    //     productDetails.append(productAuthor);
+    //     productDetails.append(priceTitle);
+    //     productDetails.append(productPrice);
+
+    //     //Add the child HTML elements of the product
+    //     productElm.append(productImage);
+    //     productElm.append(productDetails);
+
+    //     //Send all the complete individual product to the product section
+    //     productsSection.append(productElm);
+    // });
+
+    populateProducts(products);
   //Array filter method to get buttons to work (all, paid, free)
  
   document.querySelector(".products-filter label[for=all] span.product-amount").textContent = products.length;
   document.querySelector(".products-filter label[for=paid] span.product-amount").textContent = paidProducts.length;
   document.querySelector(".products-filter label[for=free] span.product-amount").textContent = freeProducts.length;
 
+  let productsFilter = document.querySelector(".products-filter");
+
+  productsFilter.addEventListener("click", function(event){
+
+    console.log(event.target.id); //GIVES YOU the id, they are paid, free all
+    if (event.target.id === "all") {
+    populateProducts(products);
+    } else if (event.target.id === "paid") {
+    populateProducts(paidProducts);
+    } else if (event.target.id === "free") {
+    populateProducts(freeProducts)
+    }
+  });
+
 }
 
-
+function footerHandler() {
+    let currentYear = new Date().getFullYear();//this is dynamic so when year changes it will update
+    document.querySelector("footer").textContent = `© ${currentYear} - All rights reserved`;
+    //© 2023 - All rights reserved
+}
 
 
 
@@ -331,5 +405,6 @@ greetingHandler();
 clockHandler();
 galleryHandler();
 productsHandler();
+footerHandler();
 
 
