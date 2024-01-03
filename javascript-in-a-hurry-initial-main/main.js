38bb2e5e9e7b76c4d903b2650f698da536c13028
oggie <morkthealien@gmail.com>
2024-01-03 11:40:58 -0600

    finished the error catch

--- javascript-in-a-hurry-initial-main/main.js
+++ javascript-in-a-hurry-initial-main/main.js
@@ -1,6 +1,4 @@
 
-
-
 //put this gallery at the top so it will be available to all functions
 const galleryImages = [
 
@@ -75,11 +73,12 @@ function celsiusToFahr(temperature) {
     let fahr = (temperature * (9/5)) + 32
     return fahr;
 }
-//invoke the function above to use it!
 
 //Greeting Section
 
 function greetingHandler() {
+
+
 let currentHour = new Date().getHours();
 
 let greetingText;
@@ -93,42 +92,62 @@ if (currentHour <12) {
     greetingText = "Welcome!";
 }
 
-//TEMPERATURE CHANGE 
-document.querySelector(".weather-group").addEventListener("click", function(e) {
+//Weather Text
+
+// Fetch method for using API for weather at location, I'm placing it in greeting handler
+navigator.geolocation.getCurrentPosition(function(position){
+
+    const latitude = position.coords.latitude;
+    
+    const longitude = position.coords.longitude;
+ 
+    console.log(latitude);
+    console.log(longitude);
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m`;
+    console.log(url);
+
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data); //<--helps to get the actual object
+        const temperature = data.current.temperature_2m;
+        console.log(temperature);//<--gets actual temperature, no location or condition because using another free API
+
+       
+        
+    //TEMPERATURE CHANGE 
+    document.querySelector(".weather-group").addEventListener("click", function(e) {
    
     if (e.target.id == "celsius") {
         document.querySelector("p#weather").innerHTML = celsiusText;
     } else if (e.target.id == "fahr") {
         document.querySelector("p#weather").innerHTML = farenheitText;
     }
+    });
+
+
+    let celsiusText = `It is ${temperature.toFixed(1)} °C outside.`;//can also use temperature.toString()
+    let farenheitText = `It is ${celsiusToFahr(temperature).toFixed(1)} °F outside.`;//can also use temperature.toString()
+
+
+
+    //send the values to the HTML elements!
+    document.querySelector("#greeting").innerHTML = greetingText;
+    document.querySelector("p#weather").innerHTML = celsiusText;
+    
+}).catch((error => {
+    document.querySelector("p#weather").innerHTML = "Unable to get the weather information. Try again later.";//test this by typing undefined for temperature const
+}));
+
 });
-
-// const greetingText = "Good morning!";
-const weatherCondition = "sunny";
-const userLocation = "New York";
-let temperature = 25;//sadly we have to set this manually right now
-let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it is ${temperature.toFixed(1)} °F outside.`;//can also use temperature.toString()
-let farenheitText = `The weather is ${weatherCondition} in ${userLocation} and it is ${celsiusToFahr(temperature).toFixed(1)} °F outside.`;//can also use temperature.toString()
-
-//send the values to the HTML elements!
-document.querySelector("#greeting").innerHTML = greetingText;
-document.querySelector("p#weather").innerHTML = celsiusText;
-
 }
 
 
-//array for the images
-//let array1 = []; also array1.length; the last element array1[array1.length - 1]
-//add another element to an array, at the end -- array1.push(item); -- add a number to the beginning, array1.unshift(item); --we can merge 2 arrays, array1.concat(array2);
 
 //objects
 student = {"name": "John", "yearOfBirth": 1980, "country": "Italy"}//key value pairs
 //undefined is the absence of a value on a variable
 //null and undefined have the same value but the TYPE is different
-
-//The given JavaScript comparison a == b, where a = "2" and b = 2, will produce a result of true. This is because the loose equality operator (==) performs type coercion, converting the operands to the same type before making the comparison. 
-
-
 
 //DATE SECTION
 new Date() //this is a Javascript object for date, will just print the date
@@ -137,25 +156,7 @@ shippingDate = new Date("2023-12-20")
 deliveryDate = new Date("2023-12-26")
 deliveryDate - shippingDate //gives you the number of milliseconds between the dates
 //divide this by 8640000 to get hours so (deliveryDate - shippingDate)/8640000
-//METHODS of the DATE OBJECT
-// new Date().getHours()
-// new Date().getMinutes()
-// new Date().getSeconds()
 
-//example: 
-{/* <body>
-    
-    <div id="local-time">
-        <span id="hours">00</span> : <span id="minutes">00</span>
-    </div>
-    
-    <script type="text/javascript" charset="utf-8">
-        // write your Javascript code below this line
-        let hours = new Date().getHours();
-        let minutes = new Date().getMinutes();
-
-    </script>
-</body> */}
 let localTime = new Date();
 document.querySelector("span[data-time=hours]").textContent = localTime.getHours();
 document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes();
@@ -182,17 +183,6 @@ document.querySelector("span[data-time=seconds]").textContent = localTime.getSec
 //GALLERY SECTION
 function galleryHandler() {
 //src: "./assets/gallery/image1.jpg alt="Thumbnail Image 1"
-
-
-
-// for (let i in galleryImages) {
-//     console.log(galleryImages[i]);
-// }
-
-//forEach Loop(a method of the array data type)
-// galleryImages.forEach(function(image, index){
-//     console.log(image); //using index AND the elements themselves
-// });
 
     
 let mainImage = document.querySelector("#gallery > img"); //this selects the element, but he created a variable to keep using query selector
@@ -242,18 +232,6 @@ thumb.addEventListener("click", function(e) {
 }
 
 
-//Products Section - will be taken from json file --- copied pasted here so we can write the loop below!!
-
-{/* <div class="product-item">
-<img src="./assets/products/img6.png" alt="AstroFiction">
-
-<div class="product-details"> 
-   <h3 class="product-title">AstroFiction</h3>
-   <p class="product-author">John Doe</p>
-   <p class="price-title">Price</p>
-   <p class="product-price">$ 49.90</p>
-    </div>
-</div> */}
 
 function productsHandler(){//run loop through products array and generate these items
 
@@ -326,53 +304,6 @@ function productsHandler(){//run loop through products array and generate these 
         return item.price > 0;
     });
 
-    // //run a loop through the products and create an HTML element ("product-item") for each of them
-    // products.forEach(function(product,index){
-    //     //Create the HTML element for the individual product
-    //     let productElm = document.createElement("div");
-    //     productElm.classList.add("product-item");
-        
-    //     //Create the product image
-    //     let productImage = document.createElement("img");
-    //     productImage.src = product.image;
-    //     productImage.alt = "Image for " + product.title;//did this because they don't have alt
-
-    //     //Create the product details section
-    //     let productDetails = document.createElement("div");
-    //     productDetails.classList.add("product-details");
-
-    //     //Create Product Title (the H3), author, price-title, and price (which are 3 Ps)
-    //     let productTitle = document.createElement("h3");
-    //     productTitle.classList.add("product-title");
-    //     productTitle.textContent = product.title;
-
-    //     //these are the P tags
-    //     let productAuthor = document.createElement("p");
-    //     productAuthor.classList.add("product-author");
-    //     productAuthor.textContent = product.author;
-
-    //     let priceTitle = document.createElement("p");
-    //     priceTitle.classList.add("price-title");
-    //     priceTitle.textContent = "Price";
-
-    //     let productPrice = document.createElement("p");
-    //     productPrice.classList.add("product-price");
-    //     productPrice.textContent = product.price > 0 ? "$" + product.price.toFixed(2) : "Free";
-
-    //     //Append the product details
-    //     productDetails.append(productTitle);
-    //     productDetails.append(productAuthor);
-    //     productDetails.append(priceTitle);
-    //     productDetails.append(productPrice);
-
-    //     //Add the child HTML elements of the product
-    //     productElm.append(productImage);
-    //     productElm.append(productDetails);
-
-    //     //Send all the complete individual product to the product section
-    //     productsSection.append(productElm);
-    // });
-
     populateProducts(products);
   //Array filter method to get buttons to work (all, paid, free)
  
@@ -401,36 +332,6 @@ function footerHandler() {
     document.querySelector("footer").textContent = `© ${currentYear} - All rights reserved`;
     //© 2023 - All rights reserved
 }
-
-
-  
-
-// Fetch method for using API for weather at location
-navigator.geolocation.getCurrentPosition(function(position){
-
-    const latitude = position.coords.latitude;
-    
-    const longitude = position.coords.longitude;
- 
-
-    console.log(latitude);
-    console.log(longitude);
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m&temperature_unit=fahrenheit`;
-    console.log(url);
-
-
-
-    fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-        let temperature = data.current.temperature_2m;
-    console.log(temperature);
-});
-});
-
-
-  
-
 
 
 
